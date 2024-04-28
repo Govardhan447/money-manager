7213319965d3d6dc4599e41c401678b77062defb
Govardhan447 <163231039+Govardhan447@users.noreply.github.com>
2024-04-28 14:05:05 +0530

    Update index.js

--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -1,14 +1,36 @@
 import './index.css'
 
 const TransactionItem = props => {
-  const {transactionListItem} = props
+  const {transactionListItem, updatedMoneyList} = props
   const {id, title, amount, type} = transactionListItem
+  const onclickDelete = () => {
+    updatedMoneyList(id)
+  }
+  const lowCase = type.toLowerCase()
+
+  const typeCase =
+    lowCase.charAt(0).toUpperCase() + lowCase.substring(1).toLowerCase()
+
   return (
-    <div className="transaction-details">
+    <li className="transaction-details">
       <p className="paragraph">{title}</p>
       <p className="paragraph">{amount}</p>
-      <p className="paragraph">{type}</p>
-    </div>
+      <p className="paragraph">{typeCase}</p>S
+      <div className="delete-container">
+        <button
+          className="delete-btn"
+          data-testid="delete"
+          type="button"
+          onClick={onclickDelete}
+        >
+          <img
+            className="delete"
+            src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
   )
 }
 
